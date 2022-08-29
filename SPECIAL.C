96be8c1c336f96d600bd9d4c40c13b51287c70bd
kinjalvegad <110524939+kinjalvegad@users.noreply.github.com>
2022-08-29 17:24:36 +0530

    Add files via upload

---
+++ SPECIAL.C
@@ -0,0 +1,15 @@
+//special oparator
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	long double a;
+	int d,b,c;
+	clrscr();
+	printf("\n var.a occupy%d bytes",size of(a));
+	d=b=c=20;
+	printf("\n b=%d\t c=%d\t d=%d",b,c,d);
+	b=(c=5,d=10,c+d);
+	printf("\n b=%d\t c=%d\t d=%d",b,c,d);
+	getch();
+}
