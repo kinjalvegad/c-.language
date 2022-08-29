6d739dd79b87e649f4ef80450717cf3b95069804
kinjalvegad <110524939+kinjalvegad@users.noreply.github.com>
2022-08-29 17:18:56 +0530

    Add files via upload

---
+++ OPERT2.C
@@ -0,0 +1,11 @@
+//conditional operator
+//input an int.wap that check whether the no.is +ve,-ve or zero
+void main()
+{
+	int a;
+	clrscr();
+	printf("Enter a no.");
+	scanf("%d",&a);
+	a>0?printf("%d is positive",a): a<0?printf("%d is negative",a):printf("%d is zero",a);
+	getch();
+}
