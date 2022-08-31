fc146f70fdee0730705cf6a274ba201a82e7f542
kinjalvegad <110524939+kinjalvegad@users.noreply.github.com>
2022-08-31 17:26:55 +0530

    Add files via upload

---
+++ ADDITION.C
@@ -0,0 +1,13 @@
+//addition of two number
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,sum;
+	clrscr();
+	printf("Enter two number:");
+	scanf("%d%d",&a,&b);
+	sum=a+b;
+	printf("\n sum of two numbers=%d",sum);
+	getch();
+}
