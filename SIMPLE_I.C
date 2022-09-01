80b8e7a7d020058aca9a950dc6f4667e57289300
kinjalvegad <110524939+kinjalvegad@users.noreply.github.com>
2022-09-01 17:37:52 +0530

    Add files via upload

---
+++ SIMPLE_I.C
@@ -0,0 +1,16 @@
+//simple if
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int age;
+	clrscr();
+	printf("Enter age");
+	scanf("%d",&age);
+	if(age>=18)
+	{
+		printf("\n you are eligible for voting");
+	}
+		printf("\n End");
+       getch();
+}
