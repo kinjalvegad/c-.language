26b73942e5b5112505f8369690fc5723985ea940
kinjalvegad <110524939+kinjalvegad@users.noreply.github.com>
2022-09-13 16:21:31 +0530

    Add files via upload

--- 1_12_123.C
+++ 1_12_123.C
@@ -1,20 +1,22 @@
-//  1
-    1_2
-    1_2_3
-    1_2_3_4
+/* 1
+   1 2
+   1 2 3
+   1 2 3 4
+*/
 #include<stdio.h>
 #include<conio.h>
 void main()
 {
-	int n ,i,j;
+	int i,j,n;
 	clrscr();
 	printf("Enter no:");
 	scanf("%d",&n);
 	for(i=1;i<=n;i++)
-		{
-			for(j=1;j<=i;j++)
-				printf("%5d",j);
-				printf("\n");
-		}
-		getch();
+	{
+	 for(j=1;j<=i;j++)
+	   printf("%5d",j);
+	   printf("\n");
+	}
+	getch();
 }
+
