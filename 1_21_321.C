74ddc6e7b758cdde1f73704a37b8104ddbd01338
kinjalvegad <110524939+kinjalvegad@users.noreply.github.com>
2022-09-13 12:47:51 +0530

    Add files via upload

---
+++ 1_21_321.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n,i,j;
+	clrscr();
+	printf("Enter no:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		for(j=i;j>=1;j--)
+		printf("%5d",j);
+		printf("\n");
+	}
+	getch();
+}
