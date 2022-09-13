fea6494cda689d2e76aa60205e81751dec33ddbf
kinjalvegad <110524939+kinjalvegad@users.noreply.github.com>
2022-09-13 15:46:12 +0530

    Add files via upload

--- 1_21_321.C
+++ 1_21_321.C
@@ -1,16 +1,17 @@
+//1_21_321 pyramid:
 #include<stdio.h>
 #include<conio.h>
 void main()
 {
-	int n,i,j;
+	int i,n,j;
 	clrscr();
 	printf("Enter no:");
 	scanf("%d",&n);
 	for(i=1;i<=n;i++)
 	{
-		for(j=i;j>=1;j--)
-		printf("%5d",j);
-		printf("\n");
+	 for(j=i;j>=1;j--)
+	  printf("%5d",j);
+	  printf("\n");
 	}
 	getch();
 }
