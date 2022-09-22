580481aa3acfcc581f5b36a3da7ff54d50fa709b
kinjalvegad <110524939+kinjalvegad@users.noreply.github.com>
2022-09-22 13:13:37 +0530

    Add files via upload

---
+++ TRIANGLE.C
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int i,n,j;
+   clrscr();
+   printf("Enter no:");
+   scanf("%d",&n);
+   for(i=n;i>=1;i--)
+   {
+    for(j=1;j<=i;j++)
+      printf("  ");
+   for(j=n;j>=i;j--)
+     printf("%4d",j);
+     printf("\n");
+   }
+   getch();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
