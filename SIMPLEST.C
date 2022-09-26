fb6ab6a7619a7e0995ca5e5e73f85e2aaa05d6a0
kinjalvegad <110524939+kinjalvegad@users.noreply.github.com>
2022-09-26 17:34:16 +0530

    Add files via upload

---
+++ SIMPLEST.C
@@ -0,0 +1,16 @@
+//string
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	char s1[20],s2[20],s3[20];
+	clrscr();
+	printf("Enter a string:");
+	scanf("%s",s1);
+	printf("Enter a string:");
+	scanf("%s",s2);
+	printf("Enter a string:");
+	scanf("%s",s3);
+	printf("\n s1=%s\ns2=%s\ns3=%s",s1,s2,s3);
+	getch();
+}
