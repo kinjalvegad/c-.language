96ccd79abf42fc75413d7dfcda0945cafa6bbf40
kinjalvegad <110524939+kinjalvegad@users.noreply.github.com>
2022-10-15 09:34:56 +0530

    Create 36.c

---
+++ 36.c
@@ -0,0 +1,11 @@
+include<stdio.h> 
+void main() 
+{ 
+  int i,n; //Variable definition 
+ printf("The first 10 natural numbers are:\n "); 
+scanf("%d",&n);
+for (i = 1; i <= n; i++) //Iteration 10 times 
+{ 
+printf("%d \t", i); //Print the number. 
+}
+ }
